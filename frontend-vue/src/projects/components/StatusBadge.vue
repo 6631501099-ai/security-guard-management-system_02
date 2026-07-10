b8091c1d7109d00a8d4cc190a2434ff1f6d6faea
6631501066 <6631501066@lamduan.mfu.ac.th>
2026-07-10 17:07:04 +0200

    ubdate StatusBadge

---
+++ frontend-vue/src/projects/components/StatusBadge.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+  status: { type: String, required: true }, // online | break | offline
+  label: { type: String, required: true },
+})
+</script>
+
+<template>
+  <span class="tag" :class="`tag--${props.status}`">
+    <span class="dot" :class="`dot--${props.status}`"></span>
+    {{ label }}
+  </span>
+</template>
